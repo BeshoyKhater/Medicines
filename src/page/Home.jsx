a26f90ebdefda79b0041323a5d5b0b190fbc6da6
BeshoyKhater <BeshoyKhater@gmail.com>
2023-03-30 06:21:30 +0200

    edit footer

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Category from '../components/home/Category'
-import Footer from '../components/home/Footer'
 import Gallery from '../components/home/Gallery'
 import Order from '../components/home/Order'
 import Product from '../components/home/Product'
+import Footer from '../components/layout/footer/Footer'
 import Header from '../components/layout/header/Header'
 
 function Home() {
